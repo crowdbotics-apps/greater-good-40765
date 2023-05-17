0ca5af109758d6ec1a5cf2310b0737cc7328461d
Crowdbotics <team@crowdbotics.com>
2023-05-17 08:02:16 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/greater_good_40765/MainActivity.java
@@ -0,0 +1,15 @@
+package com.greater_good_40765;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "greater_good_40765";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "greater-good-40765",
+  url: "https://greater-good-40765.botics.co",
+  api: "https://greater-good-40765.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
